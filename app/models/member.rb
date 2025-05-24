19f093fc29853ca37b293905723e4a9b4ee981d1
Lyndor Maselela <lyndormaselela@Lyndors-MacBook-Pro.local>
2025-05-24 22:00:32 +0200

    Assign last rank to new members and add spec
    - Add before_create callback to assign new members the last available rank
    - Update member spec to test rank assignment logic
    - Keeps ranking consistent as new members join

--- app/models/member.rb
+++ app/models/member.rb
@@ -5,8 +5,16 @@ class Member < ApplicationRecord
   has_many :matches_as_player_one, class_name: 'Match', foreign_key: 'player_one_id'
   has_many :matches_as_player_two, class_name: 'Match', foreign_key: 'player_two_id'
 
+  before_create :assign_rank
+
   def full_name
     "#{name} #{surname}"
   end
 
+  private
+
+  def assign_rank
+    max_rank = Member.maximum(:current_rank) || 0
+    self.current_rank = max_rank + 1
+  end
 end
